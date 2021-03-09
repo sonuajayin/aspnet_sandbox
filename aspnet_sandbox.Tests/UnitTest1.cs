9cbbbe47bb1707a2bc4ea63f178a376b878c28ff
Ajay Kumar Singh <a.singh.14@elsevier.com>
2021-03-09 20:02:17 +0530

    Pass Test

--- aspnet_sandbox.Tests/UnitTest1.cs
+++ aspnet_sandbox.Tests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace aspnet_sandbox.Tests
         [Fact(DisplayName="Check if the AppName Mathes with the expected")]
         public void testForAppName()
         {
-            var expectedAppName="AzurePipelines1";
+            var expectedAppName="AzurePipelines";
             var appNameService=new AppNameService();
             var actualAppName=appNameService.get();
             Assert.Equal(expectedAppName,actualAppName);
